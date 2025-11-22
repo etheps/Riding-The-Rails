b8173e1fbc2831100ea87954db18d3a6234606fa
Ethan <etheps123@gmail.com>
2025-11-22 21:42:38 +0000

    Initial Commit

---
+++ Riding The Rails/Riding_The_RailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Riding_The_RailsApp.swift
+//  Riding The Rails
+//
+//  Created by Ethan Epstein on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Riding_The_RailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
